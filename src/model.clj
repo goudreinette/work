696cea27eb5d57a473b55ed752392c63c786ca40
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-19 22:31:15 +0100

    validations

--- src/model.clj
+++ src/model.clj
@@ -1,18 +1,21 @@
 (ns model
-  (require [schema.core :refer [defschema]]
+  (require [schema.core :refer [defschema validate]]
            [hara.time :refer [now default-type]]))
+
 
 (def Date java.util.Date)
 (default-type Date)
 
+
 (defschema Session
   {:start-date Date
-   :end-date Date})
+   :end-date   Date})
 
 (defschema Job
-  {:start-date Date
-   :end-date Date
-   :sessions [Session]})
+  {:name       String
+   :start-date Date
+   :end-date   Date
+   :sessions   [Session]})
 
 (defschema Invoice
   {:date Date
