0dd3d284bbb9f28a27be9ac8c1babfadedfe905c
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-21 22:56:36 +0100

    queries

--- src/model.clj
+++ src/model.clj
@@ -3,3 +3,7 @@
 
 (defqueries "queries.sql"
   {:connection dbspec})
+
+
+(defn job-length-in-minutes [job-id]
+  (session-lengths-in-minutes-for-job job-id))
--- src/queries.sql
+++ src/queries.sql
@@ -1,4 +1,18 @@
--- name: session-length-in-minutes
-SELECT TIMESTAMPDIFF(MINUTE, start_date , end_date)
+-- name: sessions-for-job
+SELECT *
 FROM Sessions
-AS length
+NATURAL JOIN Jobs
+WHERE job_id = :job_id
+
+-- name: session-lengths-in-minutes-for-job
+SELECT TIMESTAMPDIFF(MINUTE, start_date , end_date)
+AS minutes
+FROM Sessions
+NATURAL JOIN Jobs
+WHERE job_id = :job_id
+
+
+-- name: find-job
+SELECT *
+FROM Jobs
+WHERE job_id = :job_id
