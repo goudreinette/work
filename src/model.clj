1a1040c646a8eb91aebe87980663916de25bca34
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-21 23:26:54 +0100

    job-

--- src/model.clj
+++ src/model.clj
@@ -5,9 +5,16 @@
   {:connection dbspec})
 
 
-
 (defn job-length-in-minutes [job-id]
   (->> job-id
     (session-lengths-in-minutes-for-job)
     (map :minutes)
     (reduce +)))
+
+(defn job-cost [job-id hourly-rate]
+  (->> job-id
+    (job-length-in-minutes)
+    (* (/ hourly-rate 60)) ; TODO: money library
+    (float)
+    (format "%.2f")
+    (read-string)))
