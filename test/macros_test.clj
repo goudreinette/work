eb5002b545aa569b5c06098508e8bf707136ebbc
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-22 17:20:05 +0100

    Uppercase

--- test/macros_test.clj
+++ test/macros_test.clj
@@ -1,7 +1,7 @@
 (ns macros-test
   (use midje.sweet macros))
 
-(def jobs (resource-routes 'Jobs {}))
+(defresource Jobs)
 
 (defn req [method uri]
   {:request-method method :uri uri})
@@ -10,9 +10,9 @@
   (resource-prefix 'Jobs) => "/jobs")
 
 (facts "about resource-routes"
-  (jobs (req :get "/jobs")) => (contains {:body "all"})
-  (jobs (req :get "/jobs/1")) => (contains {:body "single one"})
-  (jobs (req :get "/jobs/new")) => (contains {:body "show form"})
-  (jobs (req :post "/jobs/new")) => (contains {:body "create new, show message"})
-  (jobs (req :put "/jobs/1")) => (contains {:body "update, show message"})
-  (jobs (req :delete "/jobs/1")) => (contains {:body "delete, show message"}))
+  (Jobs (req :get "/jobs"))      => (contains {:body "all"})
+  (Jobs (req :get "/jobs/1"))    => (contains {:body "single one"})
+  (Jobs (req :get "/jobs/new"))  => (contains {:body "show form"})
+  (Jobs (req :post "/jobs/new")) => (contains {:body "create new, show message"})
+  (Jobs (req :put "/jobs/1"))    => (contains {:body "update, show message"})
+  (Jobs (req :delete "/jobs/1")) => (contains {:body "delete, show message"}))
