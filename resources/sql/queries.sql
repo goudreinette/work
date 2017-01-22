2db88af7a6dad25c8e68a189b83321bab618fe58
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-22 15:21:38 +0100

    job cost

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -9,24 +9,30 @@ FROM Jobs
 -- name: find-job
 SELECT *
 FROM Jobs
-WHERE job_id = :job_id
+WHERE job_id = :id
 
--- name: sessions-for-job
-SELECT *
+-- name: job-cost
+SELECT SUM(TIMESTAMPDIFF(MINUTE, start_date , end_date)) / 60 * :hourly_rate
+AS cost
 FROM Sessions
-NATURAL JOIN Jobs
-WHERE job_id = :job_id
+WHERE job_id = :id
 
--- name: session-lengths-in-minutes-for-job
-SELECT TIMESTAMPDIFF(MINUTE, start_date , end_date)
+-- name: job-length-in-minutes
+SELECT SUM(TIMESTAMPDIFF(MINUTE, start_date , end_date))
 AS minutes
 FROM Sessions
-NATURAL JOIN Jobs
-WHERE job_id = :job_id
+WHERE job_id = :id
+
+-- name: sessions-for-job
+SELECT *,
+TIMESTAMPDIFF(MINUTE, start_date , end_date)
+AS session_length_in_minutes
+FROM Sessions
+WHERE job_id = :id
 
 -- name: start-session!
 INSERT INTO Sessions (job_id)
-VALUES (:job_id)
+VALUES (:id)
 
 -- name: stop-session!
 UPDATE Sessions
--- src/model.clj
+++ src/model.clj
@@ -1,20 +1,6 @@
 (ns model
-  (use init yesql.core))
+  (use init yesql.core joy.macros))
 
+; Helpers
 (defqueries "sql/queries.sql"
   {:connection dbspec})
-
-
-(defn job-length-in-minutes [job-id]
-  (->> job-id
-    (session-lengths-in-minutes-for-job)
-    (map :minutes)
-    (reduce +)))
-
-(defn job-cost [job-id hourly-rate]
-  (->> job-id
-    (job-length-in-minutes)
-    (* (/ hourly-rate 60)) ; TODO: money library
-    (float)
-    (format "%.2f")
-    (read-string)))
