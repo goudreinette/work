9e84c5d85b178f4b4f49e626a2ab7c32319b2abe
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-23 18:26:40 +0100

    temporarily disabled other routes

--- src/resource_routes.clj
+++ src/resource_routes.clj
@@ -6,13 +6,13 @@
 
 (defn resource-routes [layout {:keys [name heading fetch-with save-with delete-with]}]
   (context (resource-prefix name) [:as {u :uri}]
-    (GET    "/"         []   (layout u))
-    (GET    "/:id"      [id] (layout u))
-    (GET    "/new"      []   (layout u))
-    (POST   "/new"      []   (layout u))
-    (GET    "/:id/edit" [id] (layout u))
-    (PUT    "/:id"      [id] (layout u))
-    (DELETE "/:id"      [id] (layout u))))
+    (GET    "/"         []   (layout u))))
+    ; (GET    "/:id"      [id] (layout u))
+    ; (GET    "/new"      []   (layout u))
+    ; (POST   "/new"      []   (layout u))
+    ; (GET    "/:id/edit" [id] (layout u))
+    ; (PUT    "/:id"      [id] (layout u))
+    ; (DELETE "/:id"      [id] (layout u))))
 
 
 (defn make-routes [resources & options]
