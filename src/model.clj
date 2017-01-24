39525dc02480c2606aa4dde5f3bda6f007855c90
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-24 15:14:11 +0100

    get-jobs-with-length

--- src/model.clj
+++ src/model.clj
@@ -6,3 +6,11 @@
 (defqueries "sql/queries.sql"
   {:connection {:dbtype "mysql" :dbname "work"
                 :user   "root"  :password ""}})
+
+
+
+(defn get-jobs-with-length []
+  (->>
+    (find-jobs)
+    (map #(assoc % :id (% :job_id)))
+    (map #(merge % (first (job-length-in-minutes %))))))
