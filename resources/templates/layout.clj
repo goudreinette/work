ab518a615badd73870f21fc9f49fb715460f0a16
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-25 16:09:14 +0100

    timer html

--- resources/templates/layout.clj
+++ resources/templates/layout.clj
@@ -1,15 +1,16 @@
 (ns templates.layout
   (use [hiccup core page]
-       [templates helpers top-menu resource-menu list single]))
+       [templates helpers timer top-menu resource-menu list single]))
 
 (defn includes []
   [:head
-    (include-css "/semantic.css" "/style.css")
+    (include-css "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.css" "/style.css")
     (include-js "https://code.jquery.com/jquery-3.1.1.min.js" "/selection.js")])
 
 
 (defn layout [all-resources current-resource items]
   (html
+    (timer items) ; separate me
     [:div#app
      (includes)
      (top-menu ["Save" "Discard" "Edit"])
---
+++ resources/templates/timer.clj
@@ -0,0 +1,10 @@
+(ns templates.timer
+  (use hiccup.form
+       templates.helpers))
+
+(defn timer [jobs]
+  [:form.timer {:class (active-class (some :running jobs) true)}
+   [:i.play.icon]
+   [:select
+     (for [{:keys [job_id name]} jobs]
+        [:option {:data-id job_id} name])]])
