5317040c8cbc38139fd1fd87ddd5532f243e4058
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-25 16:23:59 +0100

    timer behaviour

--- resources/public/selection.js
+++ resources/public/selection.js
@@ -7,4 +7,21 @@ $(() => {
         $('#edit').removeClass('hidden')
     })
 
+    $('.timer .icon').click((e) => {
+        var playing = $('.timer .icon').hasClass('play')
+
+        $('.timer select').toggleClass('hidden', playing)
+        $('.timer .time').toggleClass('hidden', !playing)
+        $('.timer .icon').toggleClass('stop', playing)
+        $('.timer .icon').toggleClass('play', !playing)
+    })
+
+    function updateTimer () {
+        var playing = $('.timer .icon').hasClass('play')
+        var seconds = $('.timer .time').html()
+
+        if (playing) {
+            $('.timer .time').html(seconds++)
+        }
+    }
 })
--- resources/templates/timer.clj
+++ resources/templates/timer.clj
@@ -5,6 +5,7 @@
 (defn timer [jobs]
   [:form.timer {:class (active-class (some :running jobs) true)}
    [:i.play.icon]
+   [:div.time ""]
    [:select
      (for [{:keys [job_id name]} jobs]
         [:option {:data-id job_id} name])]])
