dd41e6005357e2042ce80a142864f9a84c108247
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-25 18:38:08 +0100

    grid

--- project.clj
+++ project.clj
@@ -14,23 +14,12 @@
      [clj-time "0.13.0"]
      [clojurewerkz/money "1.9.0"]
      [prismatic/schema "1.1.3"]
-     [prone "1.1.4"]]
+     [prone "1.1.4"]
+     [selmer "1.10.5"]]
   :main init
   :target-path "target/%s"
-  :plugins [[lein-figwheel "0.5.8"]]
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["test"]
                    :dependencies [[midje "1.8.3"]
                                   [proto-repl "0.3.1"]]}}
-  :clean-targets [:target-path "out"]
-  :cljsbuild
-  { :builds [{:id "dev"
-              :source-paths ["cljs"]
-              :figwheel true
-              :compiler
-                {:main "client"
-                 :asset-path "cljs/out"
-                 :output-to  "public/js/main.js"
-                 :output-dir "public/js/out"}}]}
-  :figwheel
-  { :css-dirs ["public/css"]})
+  :clean-targets [:target-path "out"])
