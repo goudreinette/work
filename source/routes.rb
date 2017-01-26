de6045a4b5ff1ed968dd7a44f14d0f79fc189b0d
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-26 15:29:27 +0100

    Update

--- source/routes.rb
+++ source/routes.rb
@@ -2,27 +2,43 @@ get "/" do
   redirect "/sessions"
 end
 
+# Sessions
 get "/sessions" do
   @sessions = Session.all
   erb :sessions
 end
 
+# Jobs
 get "/jobs" do
   @jobs = Job.all
   erb :jobs
 end
 
+
+# Clients
 get "/clients" do
   @clients = Client.all
   erb :clients
 end
 
 get "/clients/new" do
+  @client = Client.new
   erb :form
 end
 
 post "/clients/new" do
-  'created'
+  Client.create params[:client]
+  redirect "/clients"
+end
+
+get "/clients/edit/:id" do
+  @client = Client.find params[:id]
+  erb :form
+end
+
+post "/clients/edit/:id" do
+  Client.update params[:client]
+  redirect "/clients"
 end
 
 get "/clients/:id" do
@@ -33,6 +49,7 @@ get "/clients/:id" do
 end
 
 
+# Invoices
 get "/invoices" do
 
 end
