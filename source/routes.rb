f5a663869dba92f52f85d4aeb57c5c5dc47ac02f
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-27 21:12:39 +0100

    menu active state

--- source/routes.rb
+++ source/routes.rb
@@ -3,6 +3,9 @@ require_relative "routes/jobs.rb"
 require_relative "routes/sessions.rb"
 require_relative "routes/invoices.rb"
 
+before do
+  @path = request.path
+end
 
 get "/" do
   redirect "/sessions"
