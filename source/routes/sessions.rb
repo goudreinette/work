db49cbfd2f256252d96ecf6f1c49594ca8893ba9
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-27 17:17:00 +0100

    sessions routes

--- source/routes/sessions.rb
+++ source/routes/sessions.rb
@@ -1,4 +1,18 @@
-get "/sessions" do
-  @sessions = Session.order('start_date DESC')
-  erb :sessions
+namespace "/sessions" do
+  get do
+    @sessions = Session.order('start_date DESC')
+    erb :sessions
+  end
+
+  get "/start/:id" do
+    'start'
+  end
+
+  get "/stop" do
+    'stop'
+  end
+
+  get "/:id" do
+    'single'
+  end
 end
