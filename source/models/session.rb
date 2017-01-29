7770c4606d84ced19ec6b3d3dbdb735edee012dc
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-29 17:41:57 +0100

    removed duration hack

--- source/models/session.rb
+++ source/models/session.rb
@@ -16,12 +16,8 @@ class Session < ActiveRecord::Base
     Session.where(end_date: nil).update_all(end_date: Time.new)
   end
 
-  def end_date
-    super&.+ 3600
-  end
-
   def duration_in_seconds
-    (end_date || (Time.new + 3600)) - start_date
+    (end_date || Time.new) - start_date
   end
 
 
