f26aa423fb4dbb0b6bae427893ff4fc8d4f7417f
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-29 16:17:53 +0100

    Large timer refactor

--- public/timer.js
+++ public/timer.js
@@ -1,37 +1,67 @@
 $(() => {
-    const $timer             = $('.menu .timer')
-    const {jobId, seconds}       = $timer.data()
+    const $timer           = $('.menu .timer')
+
+    if (!$timer) return
+
+    // Timer children
+    const $play         = $timer.find('.play')
+    const $stop         = $timer.find('.stop')
+    const $text         = $timer.find('.text')
+    const $jobText      = $timer.find('.job')
+    const $durationText = $timer.find('.duration')
+    const $jobSelect    = $timer.find(`.job-select`)
+
+    // Other elements
+    const $feedDuration = $('.feed .event:first-child .duration span')
+
+    // Event handlers
+    $play.click(start)
+    $stop.click(stop)
+    $jobSelect.change(switchJob)
+
+    // Show initial state
+    update()
 
 
-    $timer.find('.play').click(start)
-    $timer.find('.stop').click(stop)
+    function update () {
+        const {jobId, seconds, playing} = $timer.data()
 
+        // Timer
+        if (playing) {
+            $durationText.timer({seconds})
+            $feedDuration.timer({seconds})
+        } else {
+            $durationText.timer('remove')
+            $feedDuration.timer('remove')
+        }
 
-    if (jobId) {
-        const jobName = $timer.find(`.job-select [data-id="${jobId}"]`).text()
-        $timer.find('.job').text(jobName)
-        $timer.find('.stop, .text').removeClass('hidden')
-        $timer.find('.duration').timer({seconds})
-        $('.feed .event:first-child .duration span').timer({seconds})
-    } else {
-        $timer.find('.play, .job-select').removeClass('hidden')
+        // Job label
+        const jobId   = $jobSelect.val()
+        const jobName = $jobSelect.find(`[data-id="${jobId}"]`).text()
+        $jobText.text(jobName)
+
+        // Visibility: Elements for stopped state
+        $jobSelect.toggle(!playing)
+        $play.toggle(!playing)
+
+        // Visibility: Elements for playing state
+        $text.toggle(playing)
+        $stop.toggle(playing)
+    }
+
+    function switchJob () {
+        const jobId = $jobSelect.val()
+        $timer.data('job-id', jobId)
     }
 
     function start () {
-        // duplication
-        const jobId = $timer.find(`.job-select`).val()
-        const jobName = $timer.find(`.job-select [data-id="${jobId}"]`).text()
-        $timer.find('.job').text(jobName)
-        // ---
         $.get(`/sessions/start/${jobId}`)
-        $timer.find('.duration').timer()
-        $timer.find('.play, .stop, .text, .job-select').toggleClass('hidden')
+        $timer.data('playing', true)
     }
 
     function stop () {
         $.get(`/sessions/stop`)
-        $timer.find('.duration').timer('remove')
-        $('.feed .event:first-child .duration span').timer('remove')
-        $timer.find('.play, .stop, .text, .job-select').toggleClass('hidden')
+        $timer.data('playing', false)
+        $timer.data('seconds', 0)
     }
 })
