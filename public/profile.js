895371329ca17ca05fea9eea1992f6cea1d0e25d
reinvdwoerd <reinvanderwoerd@me.com>
2017-02-03 17:48:34 +0100

    promtp

--- public/profile.js
+++ public/profile.js
@@ -1,9 +1,11 @@
 $(() => {
     $('#profile #import').change((e) => {
-        data = new FormData()
-        data.append('file', e.target.files[0])
-        var request = new XMLHttpRequest()
-        request.open("POST", "/import")
-        request.send(data)
+        if (confirm('Importing will overwrite existing user data. Continue?')) {
+            data = new FormData()
+            data.append('file', e.target.files[0])
+            var request = new XMLHttpRequest()
+            request.open("POST", "/import")
+            request.send(data)
+        }
     })
 })
