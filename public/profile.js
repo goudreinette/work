47f532b86d44f415e07322c83f914aacffc7ef96
reinvdwoerd <reinvanderwoerd@me.com>
2017-02-03 15:19:49 +0100

    -

--- public/profile.js
+++ public/profile.js
@@ -2,7 +2,7 @@ $(() => {
     $('#profile #import').change((e) => {
         data = new FormData()
         data.append('file', e.target.files[0])
-        var request = new XMLHttpRequest();
+        var request = new XMLHttpRequest()
         request.open("POST", "/import")
         request.send(data)
     })
