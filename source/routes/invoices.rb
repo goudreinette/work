8e59c378efc0a0b45485da3994b7f563e1b68a61
reinvdwoerd <reinvanderwoerd@me.com>
2017-02-06 19:59:41 +0100

    removed redundant routes invoices

--- source/routes/invoices.rb
+++ source/routes/invoices.rb
@@ -1,30 +1,8 @@
 resource Invoice do
-  helpers do
-    def jobs_from_multiselect
+  before do
+    if params[:job_ids]
       params[:invoice][:jobs] = Job.find(params[:job_ids])
     end
-  end
-
-  get do
-    @prefix = 'invoices'
-    @header = lambda(&:no)
-    @cards = @invoices
-    @meta = lambda do |invoice|
-      "#{date invoice.date}, #{pluralize invoice.jobs.count, 'jobs'}"
-    end
-    erb :"partials/cards"
-  end
-
-  post "/new" do
-    jobs_from_multiselect
-    invoice = Invoice.create params[:invoice]
-    redirect "/invoices/#{invoice.id}"
-  end
-
-  post "/edit/:id" do
-    jobs_from_multiselect
-    Invoice.update params[:id], params[:invoice]
-    redirect "/invoices/#{params[:id]}"
   end
 
   get "/download/:id" do
